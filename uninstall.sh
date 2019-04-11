dab672c052a0a0f6e70b697d7866a37abb5113e8
Matthew VerLee <matt@doubleangels.com>
2019-04-10 21:36:24 -0600

    Changes to uninstall script.

--- uninstall.sh
+++ uninstall.sh
@@ -5,5 +5,6 @@ systemctl stop lars.service
 systemctl disable lars.service
 systemctl daemon-reload
 rm /etc/systemctl/system/lars.service
+python3 -m pip uninstall -r requirements.txt
 rm -rf "$PWD"
 echo "Uninstalled."
